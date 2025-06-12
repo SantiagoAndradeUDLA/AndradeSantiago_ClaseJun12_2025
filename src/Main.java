c493ca87ae8bc8bca52deae6c6d385055f268d7f
SantiagoAndradeUDLA <santiago.andrade.bucheli@udla.edu.ec>
2025-06-12 17:17:05 -0500

    figuras geometricas y Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import figurasGeometricas.CirculoDibujable;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        figurasGeometricas.CirculoDibujable circulo = new CirculoDibujable(100, 0, 0);
+
+        System.out.println("El perimetro del circulo es: " + circulo.getPerimetro());
+        System.out.println("El area del circulo es: " + circulo.getArea());
+
+        JFrame frame = new JFrame("Dibujar circulo");
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                circulo.dibujar2D(g);
+            }
+
+
+        };
+        panel.setPreferredSize(new Dimension(400, 400));
+        frame.add(panel);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
