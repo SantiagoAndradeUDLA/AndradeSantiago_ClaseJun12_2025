d654c0632d2c4e6abb8c3404d23652e610c8aabe
SantiagoAndradeUDLA <santiago.andrade.bucheli@udla.edu.ec>
2025-06-12 17:31:07 -0500

    Actualizacion del Main para crear y mostrar ambas figuras manteniendo toda la funcionalidad original sin modificaciones, se cambio la posicion de ciruclo y se anade cuadrado

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,37 @@
 import figurasGeometricas.CirculoDibujable;
+import figurasGeometricas.CuadradoDibujable;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        figurasGeometricas.CirculoDibujable circulo = new CirculoDibujable(100, 0, 0);
+        // Crear figuras
+        CirculoDibujable circulo = new CirculoDibujable(100, 50, 50);
+        CuadradoDibujable cuadrado = new CuadradoDibujable(100, 200, 50);
 
         System.out.println("El perimetro del circulo es: " + circulo.getPerimetro());
         System.out.println("El area del circulo es: " + circulo.getArea());
+        System.out.println("El perimetro del cuadrado es: " + cuadrado.getPerimetro());
+        System.out.println("El area del cuadrado es: " + cuadrado.getArea());
 
-        JFrame frame = new JFrame("Dibujar circulo");
+        JFrame frame = new JFrame("Dibujar figuras");
         JPanel panel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 circulo.dibujar2D(g);
+                cuadrado.dibujar2D(g);
             }
-
-
         };
+
         panel.setPreferredSize(new Dimension(400, 400));
         frame.add(panel);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-
     }
 }
-
 
 
 
